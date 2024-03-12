ef6c5d351913f9423fe7e084635e7f3d527d2bcd
AmIgOs434 <shapkin.nikolay12322@gmail.com>
2024-03-13 01:06:21 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+
+
+import React from 'react';
+import {BrowserRouter} from "react-router-dom";
+import AppRouter from "./AppRouter.js";
+import {observer} from "mobx-react-lite";
+import './App.css';
+import NavBar from './pages/NavBar.js';
+
+const App = observer(() => {
+
+
+    return (
+        <BrowserRouter>
+         
+            <AppRouter /> 
+      
+      
+        </BrowserRouter>
+        
+    );
+    
+});
 
 export default App;
---
+++ src/routes.js
@@ -0,0 +1,16 @@
+
+import { GLAV_ROUTE} from "./utils/consts";
+import GlavPages from "./pages/GlavPages";
+
+export const adminRoutes = [
+
+
+]
+
+export const publicRoutes = [
+    {
+        path: GLAV_ROUTE,
+        Component: GlavPages
+    },
+]
+
